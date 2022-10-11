998bb100c5d2437e52fdbdcfbf7fa43c31cdd402
resalatislam5 <resalatislam5@gmail.com>
2022-10-11 19:34:27 +0600

    reChart

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 const Statistics = () => {
+    const quiz = useLoaderData(); 
     return (
         <div>
-            <h1>Statistics</h1>
+            <LineChart width={730} height={250} data={quiz.data}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Legend verticalAlign="top" height={36}/>
+                <Line name="uv of pages" type="monotone" dataKey='total' stroke="#82ca9d" />
+            </LineChart>
         </div>
     );
 };
