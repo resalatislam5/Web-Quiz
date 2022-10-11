df4daf678727989e0f443083ad7aa5050c1ae4e2
resalatislam5 <resalatislam5@gmail.com>
2022-10-11 19:50:49 +0600

    footer

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
                 <div>
                     <img className='w-full rounded-lg' src='https://images.unsplash.com/photo-1516321497487-e288fb19713f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cXVpenxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt="img" />
                 </div>
-                <div className='mx-auto'>
+                <div className='lg:mx-auto mt-5 lg:mt-0'>
                     <h1 className='text-3xl font-bold'>Web Development Roadmaps</h1>
                     <p className='text-2xl font-semibold'>*Front-End Roadmap</p>
                     <p className='text-lg'>1. React</p>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-gray-200 py-6 mt-5'>
+            <h3 className='text-center text-2xl'>@ Web Quiz</h3>
+        </div>
+    );
+};
+
+export default Footer;<h3>@ Web Quiz</h3>
